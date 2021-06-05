bec04aed02c3a11bbacb187fc985e9c650e7f564
Pedro Lopes <pedrohlops@gmail.com>
2021-06-05 11:18:29 -0300

    add processed

--- media/audio/convert_all.sh
+++ media/audio/convert_all.sh
@@ -1,8 +1,7 @@
 for filename in ./*
 do
-	echo $filename
 	if [ "${filename: -4}" == ".wav" ]; then
-	  echo $filename
 	  yes | ffmpeg -i $filename -vn -ar 44100 -ac 2 -b:a 96k "${filename%.*}".mp3
+	  mv $filename processed/
 	fi
 done;
